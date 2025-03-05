b3d6756f38dfcd060821adf6a80f39e69c26f2c8
Rhinoj0sa <rhinojosaram@gmail.com>
2025-03-05 14:08:10 -0600

    primer cambio

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
+import { createElement, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root=createRoot(document.getElementById('root'))
+const reactElement=createElement("h1", null, "Hello, from create element!")
+root.render(reactElement)
+
